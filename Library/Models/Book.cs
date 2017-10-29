463b5a38c0660168883f3c4b789bfed72d572e97
kacpersw <swislocki.kacper@gmail.com>
2017-10-29 17:07:15 +0100

    Added few models

---
+++ Library/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Numer ISBN")]
+        public string ISBN { get; set; }
+
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+
+        [Display(Name = "Wypożyczona")]
+        public bool loaned { get; set; }
+
+        //todo author 
+        // todo files 
+        // todo tags
+
+    }
+}
---
+++ Library/Models/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Search
+    {
+        public int SearchId { get; set; }
+
+        public int BookId { get; set; }
+
+        [Display(Name = "Data wyszukiwania")]
+        public DateTime SearchDate { get; set; }
+
+        //todo user
+    }
+}
