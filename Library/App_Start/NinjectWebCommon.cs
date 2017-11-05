d1ca874c274453fa88d483f56cd7f78f05de40f6
kacpersw <swislocki.kacper@gmail.com>
2017-11-05 20:43:01 +0100

    Added Ninject

---
+++ Library/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,64 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Library.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Library.App_Start.NinjectWebCommon), "Stop")]
+
+namespace Library.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using System.Web.Http;
+    using Ninject.Web.Common.WebHost;
+    using Repo.Models;
+    using Ninject.Web.WebApi;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<LibraryContext>().ToSelf().InRequestScope();
+        }
+    }
+}
